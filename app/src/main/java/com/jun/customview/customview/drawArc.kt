b7fa1dc8847965ba5ef999b3ceb92284b7c18e28
Asleep_Jun <soundsleep@asleep.ai>
2021-03-31 16:43:42 +0900

    division error 해결중

--- app/src/main/java/com/jun/customview/customview/drawArc.kt
+++ app/src/main/java/com/jun/customview/customview/drawArc.kt
@@ -30,6 +30,7 @@ class drawArc @JvmOverloads constructor(
         //angle
         private const val ANGLE_START_PROGRESS_BACKGROUND = 0
         private const val ANGLE_END_PROGRESS_BACKGROUND = 360
+
         // stoke
         private const val DEFAULT_STROKE_WIDTH_DP = 5F
         private const val DEFAULT_STROKE_PG_WIDTH_DP = 10F
@@ -38,31 +39,61 @@ class drawArc @JvmOverloads constructor(
         private const val DEFAULT_LABEL_OFFSET_DP = 36F
         private const val DEFAULT_DIVISION_WIDTH_DP = 2F
         private const val SCALE_LABEL_TEXT_SIZE = 13F
+        private const val DEFAULT_DIVISION_TEXT_SIZE = 12F
 
         //color
         private const val DEFAULT_PROGRESS_BACKGROUND_COLOR = "#202633"
         private const val DEFAULT_PROGRESS_COLOR = "#7381a4"
         private const val DEFAULT_DIVISION_COLOR = "#7381a4"
+        private const val DEFAULT_DIVISION_TEXT_COLOR = "#ffffff"
 
         // ratio
         private const val BLUR_STROKE_RATIO = 3 / 8F
         private const val BLUR_RADIUS_RATIO = 1 / 4F
     }
 
+    // The progress circle ring background
+    private lateinit var progressBackgroundPaint: Paint
+    private lateinit var progressPaint: Paint
+    private lateinit var divisionPaint: Paint
+    private lateinit var divisionTextPaint: Paint
 
 
+    private var divisionOffset = 0
+    private var divisionLength = 0
+    private var divisionTextSize = 0
+
+    private var divisionShortLength = 0
+
+
+    private lateinit var circleBounds: RectF
+    private var radius: Float = 0F
+    private var center = Point(0, 0)
+    private var divisionWidth = 0
+//    private val hourLabels = listOf(12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)
+    private val hourLabels = listOf(12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)
+    private val hourText = listOf("오전 12시", "오전 6시", "오후 12시", "오후 6시")
+
+    private lateinit var sleepLayout: View
+    private lateinit var wakeLayout: View
+    private var sleepAngle = 30.0
+    private var wakeAngle = 225.0
 
     private fun init(@NonNull context: Context, @Nullable attrs: AttributeSet?) {
         // 처리가 필요
         circleBounds = RectF(100.0f, 200.0f, 600.0f, 700.0f)
 
         divisionOffset = dp2px(DEFAULT_DIVISION_OFFSET_DP)
-        divisionLength = dp2px(DEFAULT_DIVISION_LENGTH_DP)
+        divisionLength = dp2px(DEFAULT_DIVISION_LENGTH_DP) * 2
         divisionWidth = dp2px(DEFAULT_DIVISION_WIDTH_DP)
+        divisionTextSize = sp2Px(DEFAULT_DIVISION_TEXT_SIZE)
+
+        divisionShortLength = dp2px(DEFAULT_DIVISION_LENGTH_DP)
 
         var progressBgStrokeWidth = dp2px(DEFAULT_STROKE_WIDTH_DP)
         var progressBackgroundColor = Color.parseColor(DEFAULT_PROGRESS_BACKGROUND_COLOR)
         var divisionColor = Color.parseColor(DEFAULT_DIVISION_COLOR)
+        var divisionTextColor = Color.parseColor(DEFAULT_DIVISION_TEXT_COLOR)
         var sleepLayoutId = 0
         var wakeLayoutId = 0
 
@@ -76,14 +107,11 @@ class drawArc @JvmOverloads constructor(
             wakeLayoutId = a.getResourceId(R.styleable.customviewpractice_wakeLayoutId, 0)
         }
 
-
         progressBackgroundPaint = Paint()
         progressBackgroundPaint?.style = Paint.Style.STROKE
         progressBackgroundPaint?.strokeWidth = progressBgStrokeWidth.toFloat()
         progressBackgroundPaint?.color = progressBackgroundColor
         progressBackgroundPaint?.isAntiAlias = true
-
-
 
         progressPaint = Paint()
         progressPaint?.style = Paint.Style.STROKE
@@ -98,6 +126,12 @@ class drawArc @JvmOverloads constructor(
         divisionPaint.style = Paint.Style.STROKE
         divisionPaint.isAntiAlias = true
 
+        divisionTextPaint = Paint(0)
+        divisionTextPaint.textSize = divisionTextSize.toFloat()
+        divisionTextPaint.color = divisionTextColor
+        divisionTextPaint.isAntiAlias = true
+
+
         val inflater = LayoutInflater.from(context)
         sleepLayout = inflater.inflate(sleepLayoutId, this, false)
         wakeLayout = inflater.inflate(wakeLayoutId, this, false)
@@ -108,27 +142,6 @@ class drawArc @JvmOverloads constructor(
         setLayerType(View.LAYER_TYPE_SOFTWARE, null);
         setWillNotDraw(false)
     }
-
-
-    // The progress circle ring background
-    private lateinit var progressBackgroundPaint: Paint
-    private lateinit var progressPaint: Paint
-    private lateinit var divisionPaint: Paint
-
-
-    private var divisionOffset = 0
-    private var divisionLength = 0
-
-    private lateinit var circleBounds: RectF
-    private var radius: Float = 0F
-    private var center = Point(0, 0)
-    private var divisionWidth = 0
-    private val hourLabels = listOf(12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)
-
-    private lateinit var sleepLayout: View
-    private lateinit var wakeLayout: View
-    private var sleepAngle = 30.0
-    private var wakeAngle = 225.0
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         calculateBounds(w, h)
@@ -160,6 +173,15 @@ class drawArc @JvmOverloads constructor(
     private fun dp2px(dp: Float): Int {
         val metrics = resources.displayMetrics
         return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, metrics).toInt()
+    }
+
+    private fun sp2Px(sp: Float): Int {
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_SP,
+            sp,
+            resources.displayMetrics
+        )
+            .toInt()
     }
 
     private fun isTouchOnView(view: View, ev: MotionEvent): Boolean {
@@ -243,23 +265,88 @@ class drawArc @JvmOverloads constructor(
     }
 
     private fun drawDivisions(canvas: Canvas) {
-        val divisionAngle = 360 / hourLabels.size
-        hourLabels.forEachIndexed { index, value ->
+//        val divisionAngle = 360 / hourLabels.size
+//        hourLabels.forEachIndexed { index, value ->
+//            val angle = (divisionAngle * index) - 90
+//            val radians = Math.toRadians(angle.toDouble())
+//            val bgStrokeWidth = progressBackgroundPaint.strokeWidth
+//            val startX = center.x + (radius - bgStrokeWidth / 2 - divisionOffset) * cos(radians)
+//            val endX =
+//                center.x + (radius - bgStrokeWidth / 2 - divisionOffset - divisionLength) * cos(
+//                    radians
+//                )
+//            val startY = center.y + (radius - bgStrokeWidth / 2 - divisionOffset) * sin(radians)
+//            val endY =
+//                center.y + (radius - bgStrokeWidth / 2 - divisionOffset - divisionLength) * sin(
+//                    radians
+//                )
+//            canvas.drawLine(
+//                startX.toFloat(),
+//                startY.toFloat(),
+//                endX.toFloat(),
+//                endY.toFloat(),
+//                divisionPaint
+//            )
+//        }
+        val divisionAngle = 360 / 60
+        for (index in 0..59) {
+            Log.d(TAG, index.toString())
             val angle = (divisionAngle * index) - 90
-//            Log.d(TAG, "drawDivisions $angle $value")
             val radians = Math.toRadians(angle.toDouble())
             val bgStrokeWidth = progressBackgroundPaint.strokeWidth
             val startX = center.x + (radius - bgStrokeWidth / 2 - divisionOffset) * cos(radians)
-            val endX = center.x + (radius - bgStrokeWidth / 2 - divisionOffset - divisionLength) * cos(radians)
+            val endX =
+                center.x + (radius - bgStrokeWidth / 2 - divisionOffset - divisionLength) * cos(
+                    radians
+                )
             val startY = center.y + (radius - bgStrokeWidth / 2 - divisionOffset) * sin(radians)
-            val endY = center.y + (radius - bgStrokeWidth / 2 - divisionOffset - divisionLength) * sin(radians)
-            canvas.drawLine(startX.toFloat(), startY.toFloat(), endX.toFloat(), endY.toFloat(), divisionPaint)
-//            val tmp = value.toString()
-//            textPaint.getTextBounds(tmp, 0, tmp.length, textRect)
-//            val x = center.x + (radius - bgStrokeWidth / 2 - labelOffset) * cos(radians) - textRect.width() / 2
-//            val y = (center.y + (radius - bgStrokeWidth / 2 - labelOffset) * sin(radians) + textRect.height() / 2)
-//            canvas.drawText(tmp, x.toFloat(), y.toFloat(), textPaint)
+            val endY =
+                center.y + (radius - bgStrokeWidth / 2 - divisionOffset - divisionLength) * sin(
+                    radians
+                )
+            if (index % 5 == 0) {
+                divisionLength = dp2px(DEFAULT_DIVISION_LENGTH_DP) * 2
+                canvas.drawLine(
+                    startX.toFloat(),
+                    startY.toFloat(),
+                    endX.toFloat(),
+                    endY.toFloat(),
+                    divisionPaint
+                )
+            } else {
+                divisionLength = dp2px(DEFAULT_DIVISION_LENGTH_DP)
+                canvas.drawLine(
+                    startX.toFloat(),
+                    startY.toFloat(),
+                    endX.toFloat(),
+                    endY.toFloat(),
+                    divisionPaint
+                )
+            }
+
+            canvas.rotate(360F, startX.toFloat(), startY.toFloat())
+
         }
+
+        // text
+//        val divisionTextAngle = 360 / hourText.size
+//        hourText.forEachIndexed { index, value ->
+//            val angle = (divisionTextAngle * index) - 90
+//            val radians = Math.toRadians(angle.toDouble())
+//            val bgStrokeWidth = divisionTextPaint.strokeWidth
+//            val startX = center.x + (radius - bgStrokeWidth / 2 - divisionOffset) * cos(radians)
+//            val endX =
+//                center.x + (radius - bgStrokeWidth / 2 - divisionOffset - divisionLength) * cos(
+//                    radians
+//                )
+//            val startY = center.y + (radius - bgStrokeWidth / 2 - divisionOffset) * sin(radians)
+//            val endY =
+//                center.y + (radius - bgStrokeWidth / 2 - divisionOffset - divisionLength) * sin(
+//                    radians
+//                )
+//            canvas.drawText(value, startX.toFloat(), startY.toFloat(), divisionTextPaint)
+//            Log.d(TAG, "forEach 11 ")
+//        }
 
 
     }
@@ -276,10 +363,9 @@ class drawArc @JvmOverloads constructor(
     }
 
     override fun onDraw(canvas: Canvas) {
+        drawDivisions(canvas)
         drawProgressBackground(canvas)
         drawProgress(canvas)
-        drawDivisions(canvas)
-
         super.onDraw(canvas)
     }
 
